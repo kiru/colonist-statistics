b39a6549edec092bc1cbf946d71ecd2108e690ea
Kirusanth Poopalasingam <code@kiru.io>
2021-07-21 16:09:21 +0200

    Upon error reconnect WS

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from threading import Thread
 from sqlite3worker import Sqlite3Worker
 
 # The Sqlite3Worker passes sqlite requests through a queue
-sql_worker = Sqlite3Worker("test.sqlite")
+sql_worker = Sqlite3Worker("colonist.sqlite")
 
 # ensure the table are there
 sql_worker.execute("""
@@ -60,53 +60,58 @@ def track_colonist():
     last_fetch_from_catan = datetime.now()
     first_fetch = True
 
-    # We need to first get the jwt cookie from the website
-    response = requests.get("https://colonist.io")
-    cookies = response.cookies.get_dict()
-    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
-              "Sec-WebSocket-Extensions": "permessage-deflate",
-              "Sec-WebSocket-Version": "13"}
-
-    # websocket.enableTrace(True)
-    print("Connect to WebSocket")
-    ws = websocket.WebSocket()
-
-    ws.connect("ws://socket.colonist.io", cookie=f"jwt={cookies['jwt']}",
-               origin="https://colonist.io", host="socket.colonist.io", header=header)
-
-    # Send the first message so that they can start sending us data back
-    ws.send_binary(msgpack.packb({"id": "1", "data": True}))
-
-    print("Listening to messages")
     while True:
-        data = msgpack.unpackb(ws.recv())
+        # We need to first get the jwt cookie from the website
+        response = requests.get("https://colonist.io")
+        cookies = response.cookies.get_dict()
+        header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+                  "Sec-WebSocket-Extensions": "permessage-deflate",
+                  "Sec-WebSocket-Version": "13"}
 
-        # this is the nr-user-message
-        if data[b'id'] == b"49":
+        # websocket.enableTrace(True)
+        print("Connect to WebSocket")
+        ws = websocket.WebSocket()
 
-            current_date_time = datetime.now()
-            # Ping every 5 min
-            if (((current_date_time - last_fetch_from_catan).total_seconds() > 5 * 60) or first_fetch):
-                catan_response = requests.get('https://steamcharts.com/app/544730/chart-data.json')
-                insert(catan_response.json())
+        ws.connect("ws://socket.colonist.io", cookie=f"jwt={cookies['jwt']}",
+                   origin="https://colonist.io", host="socket.colonist.io", header=header)
 
-            # Store only the messages every 5min
-            if ((current_date_time - last_insert).total_seconds() > 5 * 60 or first_fetch):
-                actual_data = data[b'data']
-                last_insert = current_date_time
-                sql_worker.execute("INSERT into colonist_count values (?, ?, ?, ?, ?, ?, ?, ?)", (
-                    current_date_time,
-                    actual_data[b'userCountWithSocket'],
-                    actual_data[b'userCountInRooms'],
-                    actual_data[b'userCountInLobby'],
-                    actual_data[b'userCountInGame'],
-                    actual_data[b'userCountInSpectating'],
-                    actual_data[b'roomCount'],
-                    actual_data[b'gameCount']
-                ))
-                print(data)
+        # Send the first message so that they can start sending us data back
+        ws.send_binary(msgpack.packb({"id": "1", "data": True}))
 
-            first_fetch = False
+        print("Listening to messages")
+        try:
+            while True:
+                data = msgpack.unpackb(ws.recv())
+
+                # this is the nr-user-message
+                if data[b'id'] == b"49":
+
+                    current_date_time = datetime.now()
+                    # Ping every 5 min
+                    if ((current_date_time - last_fetch_from_catan).total_seconds() > 5 * 60) or first_fetch:
+                        catan_response = requests.get('https://steamcharts.com/app/544730/chart-data.json')
+                        insert(catan_response.json())
+
+                    # Store only the messages every 5min
+                    if (current_date_time - last_insert).total_seconds() > 5 * 60 or first_fetch:
+                        actual_data = data[b'data']
+                        last_insert = current_date_time
+                        sql_worker.execute("INSERT into colonist_count values (?, ?, ?, ?, ?, ?, ?, ?)", (
+                            current_date_time,
+                            actual_data[b'userCountWithSocket'],
+                            actual_data[b'userCountInRooms'],
+                            actual_data[b'userCountInLobby'],
+                            actual_data[b'userCountInGame'],
+                            actual_data[b'userCountInSpectating'],
+                            actual_data[b'roomCount'],
+                            actual_data[b'gameCount']
+                        ))
+                        print(data)
+
+                    first_fetch = False
+        except Exception as err:
+            exception_type = type(err).__name__
+            print("Failed: ", exception_type)
 
 
 app = Flask(__name__)
@@ -129,7 +134,7 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    # Run web scoket handling in a separat thread
+    # Run web socket handling in a separate thread
     thread = Thread(target=track_colonist)
     thread.start()
 
